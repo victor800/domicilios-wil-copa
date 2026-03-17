9751f18604a536563502905218f48fd0b77620f0
victor800 <hincapiealfonso628@gmail.com>
2026-03-17 15:45:34 -0500

    fix: start.sh escribe credentials correctamente

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-echo $GOOGLE_CREDENTIALS > credentials.json
+echo $GOOGLE_CREDENTIALS | node -e "process.stdin.resume();process.stdin.on('data',d=>require('fs').writeFileSync('credentials.json',d))"
 node index.js
