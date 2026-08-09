6d63808e902fb3fc152ab6edf0976d30a8767de0
victor800 <hincapiealfonso628@gmail.com>
2026-08-08 19:02:48 -0500

    Fix: agregar @OptIn(ExperimentalForeignApi) para APIs de interop iOS

--- shared/src/iosMain/kotlin/com/domicilioswil/shared/ui/components/PharmacyCardActual.kt
+++ shared/src/iosMain/kotlin/com/domicilioswil/shared/ui/components/PharmacyCardActual.kt
@@ -8,16 +8,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.interop.UIKitView
 import androidx.compose.ui.unit.dp
+import kotlinx.cinterop.ExperimentalForeignApi
 import platform.Foundation.NSURL
 import platform.Foundation.NSURLRequest
 import platform.UIKit.UIApplication
 import platform.WebKit.WKWebView
 
+@OptIn(ExperimentalForeignApi::class)
 actual fun openUrl(url: String) {
     val nsUrl = NSURL.URLWithString(url) ?: return
     UIApplication.sharedApplication.openURL(nsUrl)
 }
 
+@OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun MapEmbedView(embedUrl: String, modifier: Modifier) {
     UIKitView(
